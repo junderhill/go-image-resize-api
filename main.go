463ded91a1b9f839df24c373312c13edd3f26fb6
Jason Underhill <jason@dev48.co.uk>
2022-03-30 17:00:04 +0100

    refactor handler

--- main.go
+++ main.go
@@ -15,31 +15,33 @@ import (
 func main() {
 	port := 8080
 
-	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("%s /image route requested", r.Method)
-
-		keys, ok := r.URL.Query()["size"]
-		if !ok || len(keys[0]) < 1 {
-			http.Error(w, "Missing `size` query string parameter", http.StatusBadRequest)
-			return
-		}
-
-		sizeInt, err := strconv.Atoi(keys[0])
-		if err != nil {
-			fmt.Println(err)
-			fmt.Fprintln(w, err)
-			http.Error(w, "Invalid `size` parameter", http.StatusBadRequest)
-			return
-		}
-
-		resizedImage := getImage("images/01.jpg", uint(sizeInt))
-		fmt.Fprintf(w, "%s", resizedImage)
-	})
+	http.HandleFunc("/image", imageHandler)
 
 	fmt.Printf("Starting server on port %d", port)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s /image route requested", r.Method)
+
+	keys, ok := r.URL.Query()["size"]
+	if !ok || len(keys[0]) < 1 {
+		http.Error(w, "Missing `size` query string parameter", http.StatusBadRequest)
+		return
+	}
+
+	sizeInt, err := strconv.Atoi(keys[0])
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintln(w, err)
+		http.Error(w, "Invalid `size` parameter", http.StatusBadRequest)
+		return
+	}
+
+	resizedImage := getImage("images/01.jpg", uint(sizeInt))
+	fmt.Fprintf(w, "%s", resizedImage)
 }
 
 func getImage(filename string, size uint) []byte {
